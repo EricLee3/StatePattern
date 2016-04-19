0a21cce2acb2162280fdae44b234cded5a3b8c8a
Eric Lee <psyche22@gmail.com>
2016-04-19 22:38:43 +0900

    first commit

---
+++ src/SoldOutState.java
@@ -0,0 +1,24 @@
+
+public class SoldOutState implements State {
+	GumballMachine gumballMachine;
+	
+	public SoldOutState(GumballMachine gumballMachine)
+	{
+		this.gumballMachine = gumballMachine;
+	}
+	public void insertQuarter()
+	{
+		
+	}
+	public void ejectQuarter()  {
+	
+	}
+	public void turnCrank()  {
+		
+	}
+	public void dispense()  {
+		
+	}
+	
+	
+}
